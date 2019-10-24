5e48109332bce3dd6d29d5c2d36d1fb4ef77116f
Eric Kwok <46385457+Kwok-Eric-216397150@users.noreply.github.com>
2019-10-24 19:19:28 -0400

    Lab 4

--- EECS2030_Lab3/src/eecs2030/lab3/Complex.java
+++ EECS2030_Lab3/src/eecs2030/lab3/Complex.java
@@ -11,41 +11,35 @@ import java.util.List;
  */
 public final class Complex {
 
+	public static final Complex ONE = new Complex(1.0, 0.0);
+	public static final Complex I = new Complex(0.0, 1.0);
+	private final double re;
+	private final double im;
 
-public static final Complex ONE = new Complex(1.0, 0.0);
-public static final Complex I = new Complex(0.0, 1.0);
-private final double re;
-private final double im;
-
-	
-	
 	/**
 	 * Initializes this complex number to <code>0 + 0i</code>.
 	 * 
 	 */
 	public Complex() {
-	this(0, 0);
+		this(0, 0);
 	}
 
 	/**
-	 * Initializes this complex number so that it has the same real and
-	 * imaginary parts as another complex number.
+	 * Initializes this complex number so that it has the same real and imaginary
+	 * parts as another complex number.
 	 * 
-	 * @param other
-	 *            the complex number to copy.
+	 * @param other the complex number to copy.
 	 */
 	public Complex(Complex other) {
-	this(other.re, other.im);
+		this(other.re, other.im);
 	}
 
 	/**
-	 * Initializes this complex number so that it has the given real
-	 * and imaginary components.
+	 * Initializes this complex number so that it has the given real and imaginary
+	 * components.
 	 * 
-	 * @param re
-	 *            the real part of the complex number.
-	 * @param im
-	 *            the imaginary part of the complex number.
+	 * @param re the real part of the complex number.
+	 * @param im the imaginary part of the complex number.
 	 */
 	public Complex(double re, double im) {
 		this.re = re;
@@ -53,31 +47,29 @@ private final double im;
 	}
 
 	/**
-	 * A static factory method that returns a new complex number whose real part
-	 * is equal to re and whose imaginary part is equal to 0.0
+	 * A static factory method that returns a new complex number whose real part is
+	 * equal to re and whose imaginary part is equal to 0.0
 	 * 
-	 * @param re
-	 *            the desired real part of the complex number
+	 * @param re the desired real part of the complex number
 	 * @return a new complex number whose real part is equal to re and whose
 	 *         imaginary part is equal to 0.0
 	 */
 	public static Complex real(double re) {
-	Complex c = new Complex(re, 0.0);
+		Complex c = new Complex(re, 0.0);
 		return c;
 	}
 
 	/**
-	 * A static factory method that returns a new complex number whose real part
-	 * is equal to 0.0 and whose imaginary part is equal to im
+	 * A static factory method that returns a new complex number whose real part is
+	 * equal to 0.0 and whose imaginary part is equal to im
 	 * 
-	 * @param im
-	 *            the desired imaginary part of the complex number
+	 * @param im the desired imaginary part of the complex number
 	 * @return a new complex number whose real part is equal to 0.0 and whose
 	 *         imaginary part is equal to im
 	 */
 	public static Complex imag(double im) {
-	Complex c = new Complex(0.0, im);
-	
+		Complex c = new Complex(0.0, im);
+
 		return c;
 	}
 
@@ -87,7 +79,7 @@ private final double im;
 	 * @return the real part of the complex number.
 	 */
 	public double re() {
-		
+
 		return this.re;
 	}
 
@@ -97,49 +89,43 @@ private final double im;
 	 * @return the imaginary part of the complex number.
 	 */
 	public double im() {
-		
+
 		return this.im;
 	}
 
 	/**
-	 * Add this complex number and another complex number to obtain a new
-	 * complex number. Neither this complex number nor c is changed by
-	 * this method.
+	 * Add this complex number and another complex number to obtain a new complex
+	 * number. Neither this complex number nor c is changed by this method.
 	 * 
-	 * @param c
-	 *            The complex number to add to this complex number.
-	 * @return a new Complex number equal to the sum of this complex number and
-	 *         c.
+	 * @param c The complex number to add to this complex number.
+	 * @return a new Complex number equal to the sum of this complex number and c.
 	 */
 	public Complex add(Complex c) {
-		//(a+bi)+(c+di)=(a+c)+(b+d)i
+		// (a+bi)+(c+di)=(a+c)+(b+d)i
 		double newre = this.re + c.re;
-	    double newim = this.im + c.im;
-	    Complex d = new Complex(newre, newim);
+		double newim = this.im + c.im;
+		Complex d = new Complex(newre, newim);
 		return d;
 	}
 
 	/**
 	 * Multiply this complex number with another complex number to obtain a new
-	 * complex number. Neither this complex number nor c is changed by
-	 * this method.
+	 * complex number. Neither this complex number nor c is changed by this method.
 	 * 
 	 * <p>
 	 * This is not an industrial strength implementation of complex number
 	 * multiplication. In particular, issues related to the differences between
-	 * <code>-0.0</code> and <code>0.0</code> and infinite real or imaginary
-	 * parts are not taken into account.
+	 * <code>-0.0</code> and <code>0.0</code> and infinite real or imaginary parts
+	 * are not taken into account.
 	 * 
-	 * @param c
-	 *            The complex number to multiply by.
-	 * @return a new Complex number equal to this complex number multiplied by
-	 *         c.
+	 * @param c The complex number to multiply by.
+	 * @return a new Complex number equal to this complex number multiplied by c.
 	 */
 	public Complex multiply(Complex c) {
-    //(a+bi)×(c+di)=((a×c)−(b×d))+((b×c)+(a×d))i
-    double newre = (this.re * c.re) -(this.im * c.im);
-    double newim = (this.im * c.re) + (this.re * c.im);
-    Complex d = new Complex(newre, newim);
+		// (a+bi)×(c+di)=((a×c)−(b×d))+((b×c)+(a×d))i
+		double newre = (this.re * c.re) - (this.im * c.im);
+		double newim = (this.im * c.re) + (this.re * c.im);
+		Complex d = new Complex(newre, newim);
 		return d;
 	}
 
@@ -149,27 +135,27 @@ private final double im;
 	 * @return the magnitude of this complex number.
 	 */
 	public double mag() {
-		//	|(a+bi)|=√a×a+b×b
-		if(this.re != Double.MAX_VALUE && this.im != Double.MAX_VALUE) {
-		double absre = Math.abs(this.re);
-		double absim = Math.abs(this.im);
-		return  Math.hypot(absre, absim);
+		// |(a+bi)|=√a×a+b×b
+		if (this.re != Double.MAX_VALUE && this.im != Double.MAX_VALUE) {
+			double absre = Math.abs(this.re);
+			double absim = Math.abs(this.im);
+			return Math.hypot(absre, absim);
 		}
-	 return Double.MAX_VALUE;	
+		return Double.MAX_VALUE;
 	}
 
 	/**
 	 * Return a hash code for this complex number.
 	 * 
 	 * <p>
-	 * This implementation uses a very crude algorithm to compute
-	 * the hash code; the hash code is computed as follows:
+	 * This implementation uses a very crude algorithm to compute the hash code; the
+	 * hash code is computed as follows:
 	 * 
 	 * <ol>
-	 * <li>compute the value equal to <code>9999</code> times the real part
-	 *     of this complex number
-	 * <li>compute the value equal to <code>99</code> times the imaginary part
-	 *     of this complex number
+	 * <li>compute the value equal to <code>9999</code> times the real part of this
+	 * complex number
+	 * <li>compute the value equal to <code>99</code> times the imaginary part of
+	 * this complex number
 	 * <li>compute the sum of the values computed in Steps 1 and 2
 	 * <li>casts the value computed in Step 3 to an <code>int</code>
 	 * <li>returns the value computed in Step 4
@@ -183,9 +169,9 @@ private final double im;
 	 */
 	@Override
 	public int hashCode() {
-	double one = 9999 * this.re;
-	double two = 99 * this.im;
-	int sum = (int) (one + two); 
+		double one = 9999 * this.re;
+		double two = 99 * this.im;
+		int sum = (int) (one + two);
 		return sum;
 	}
 
@@ -194,10 +180,9 @@ private final double im;
 	 * <code>true</code> if and only if the argument is a <code>Complex</code>
 	 * number with the same real and imaginary parts as this complex number.
 	 * 
-	 * @param obj
-	 *            the object to compare this Complex number against.
-	 * @return true if the given object is a Complex number equal to this
-	 *         complex number, false otherwise.
+	 * @param obj the object to compare this Complex number against.
+	 * @return true if the given object is a Complex number equal to this complex
+	 *         number, false otherwise.
 	 */
 	@Override
 	public boolean equals(Object obj) {
@@ -211,7 +196,7 @@ private final double im;
 			return false;
 		}
 		Complex other = (Complex) obj;
-		if(this.re != other.re || this.im != other.im) {
+		if (this.re != other.re || this.im != other.im) {
 			return false;
 		}
 		return true;
@@ -223,77 +208,75 @@ private final double im;
 	 * <p>
 	 * The returned string is the real part of the complex number, followed by a
 	 * space, followed by a <code>+</code> or <code>-</code> sign, followed by a
-	 * space, followed by the absolute value of the imaginary part of the
-	 * complex number, followed by an <code>i</code>. The sign is <code>+</code>
-	 * if the imaginary part of the complex number is positive, and
-	 * <code>-</code> if the imaginary part of the complex number is negative.
+	 * space, followed by the absolute value of the imaginary part of the complex
+	 * number, followed by an <code>i</code>. The sign is <code>+</code> if the
+	 * imaginary part of the complex number is positive, and <code>-</code> if the
+	 * imaginary part of the complex number is negative.
 	 * 
-	 * For example the complex number with real and imaginary parts equal to
-	 * zero has the string representation <code>0.0 + 0.0i</code>. The complex
-	 * number with real part equal to zero and imaginary part equal to
-	 * <code>-1</code> has the string representation <code>0.0 - 1.0i</code>.
+	 * For example the complex number with real and imaginary parts equal to zero
+	 * has the string representation <code>0.0 + 0.0i</code>. The complex number
+	 * with real part equal to zero and imaginary part equal to <code>-1</code> has
+	 * the string representation <code>0.0 - 1.0i</code>.
 	 * 
 	 * @return a string representation of this complex number.
 	 * 
 	 */
 	@Override
 	public String toString() {
-	String i = "i";
-	String sign = "";
-	double nosign = this.im;
-	if(this.im < 0) {
-    nosign = this.im * -1;
-	sign = "-";
-	}
-	if(this.im >= 0) {
-    sign = "+";
-	}
+		String i = "i";
+		String sign = "";
+		double nosign = this.im;
+		if (this.im < 0) {
+			nosign = this.im * -1;
+			sign = "-";
+		}
+		if (this.im >= 0) {
+			sign = "+";
+		}
 		return this.re + " " + sign + " " + nosign + i;
 	}
 
 	/**
-	 * Returns a complex number holding the value represented by the given
-	 * string.
+	 * Returns a complex number holding the value represented by the given string.
 	 * 
 	 * <p>
 	 * <code>valueOf</code> tries to create a complex number from a string
-	 * representation of the complex number. Strings that can interpreted as
-	 * complex numbers are those strings returned by
-	 * <code>Complex.toString</code>.
+	 * representation of the complex number. Strings that can interpreted as complex
+	 * numbers are those strings returned by <code>Complex.toString</code>.
 	 * 
-	 * @param s
-	 *            a string representation of a complex number.
-	 * @return a Complex number equal to the complex number represented by the
-	 *         given string.
-	 * @throws IllegalArgumentException
-	 *             if the string cannot be interpreted as a complex number.
-	 * @pre. s has a space before and after the + or - sign separating the
-	 *       real and imaginary parts of the complex number
+	 * @param s a string representation of a complex number.
+	 * @return a Complex number equal to the complex number represented by the given
+	 *         string.
+	 * @throws IllegalArgumentException if the string cannot be interpreted as a
+	 *                                  complex number. @pre. s has a space before
+	 *                                  and after the + or - sign separating the
+	 *                                  real and imaginary parts of the complex
+	 *                                  number
 	 */
 	public static Complex valueOf(String s) {
 		Complex result = null;
 		double in = 0.0;
 		String t = s.trim();
 		List<String> parts = Arrays.asList(t.split("\\s+"));
-		if(parts.size() != 3) {
+		if (parts.size() != 3) {
 			throw new IllegalArgumentException();
 		}
-		
+
 		String ii = parts.get(2);
 		char subii = ii.charAt(ii.length() - 1);
-		if((!parts.get(1).equals("-") && !parts.get(1).equals("+")) || subii != 'i') {
+		if ((!parts.get(1).equals("-") && !parts.get(1).equals("+")) || subii != 'i') {
 			throw new IllegalArgumentException();
 		}
 		double re = Double.valueOf(parts.get(0));
-		if(parts.get(1).contentEquals("+")) {
-		String inwithi = parts.get(2);
-		String sub = inwithi.substring(0, inwithi.length() - 1);
-		in = Double.valueOf(sub);
+		if (parts.get(1).contentEquals("+")) {
+			String inwithi = parts.get(2);
+			String sub = inwithi.substring(0, inwithi.length() - 1);
+			in = Double.valueOf(sub);
 		}
-	    if(parts.get(1).contentEquals("-")) {
-		String inwithi = parts.get(2);
-		String sub = inwithi.substring(0, inwithi.length() - 1);	
-		in = Double.valueOf(sub) * -1.0;
+		if (parts.get(1).contentEquals("-")) {
+			String inwithi = parts.get(2);
+			String sub = inwithi.substring(0, inwithi.length() - 1);
+			in = Double.valueOf(sub) * -1.0;
 		}
 		// split splits the string s by looking for spaces in s.
 		// If s is a string that might be interpreted as a complex number
@@ -309,25 +292,23 @@ private final double im;
 		// -check if the second element of parts is "+" or "-"
 		// -check if the third element of parts ends with an "i"
 		// -if any of the 3 checks are false then s isn't a complex number
-		//  and you should throw an exception
+		// and you should throw an exception
 		// -if all of the 3 checks are true then s might a complex number
 		// -try to convert the first element of parts to a double value
-		//  (use Double.valueOf); this might fail in which case s isn't
-		//  a complex number
+		// (use Double.valueOf); this might fail in which case s isn't
+		// a complex number
 		// -remove the 'i' from the third element of parts and try
-		//  to convert the resulting string to a double value
-	    //  (use Double.valueOf); this might fail in which case s isn't
-		//  a complex number
+		// to convert the resulting string to a double value
+		// (use Double.valueOf); this might fail in which case s isn't
+		// a complex number
 		// -you now have real and imaginary parts of the complex number
-		//  but you still have to account for the "+" or "-" which
-		//  is stored as the second element of parts
+		// but you still have to account for the "+" or "-" which
+		// is stored as the second element of parts
 		// -once you account for the sign, you can return the correct
-		//  complex number
+		// complex number
 		result = new Complex(re, in);
-		
-		
+
 		return result;
 	}
-
 
 }
---
+++ EECS2030_Lab4/src/eecs2030/lab4/LZWCompressor.java
@@ -0,0 +1,243 @@
+package eecs2030.lab4;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * A simple class to implement LZW compression (encoding) and decompression
+ * (decoding) of a given input string.
+ * 
+ * 
+ * <p>
+ * The LZWCompressor maintains a list of characters representing an input
+ * sequence to be encoded/decoded. It also maintains a local LZWDictionary,
+ * which is initialized with unique characters from the input sequence, and is
+ * used to encode/decode the input sequence.
+ * <p>
+ * 
+ * <p>
+ * Class invariant: The LZWDictionary always holds only the initial single
+ * character patterns as its entries (i.e. it may grow during encode/decode
+ * operations, however must be reset to its initial state after an encode/decode
+ * operation).
+ * </p>
+ * 
+ * 
+ * @author eecs2030
+ *
+ */
+public class LZWCompressor {
+
+	// FIELDS
+	// input sequence to be encoded
+	// dictionary to use when encoding/decoding
+	String input;
+	LZWDictionary d;
+	List<String> enc;
+
+	/**
+	 * Initialize this LZWCompressor to encode/decode a specified input string.
+	 * 
+	 * 
+	 * <p>
+	 * A list of characters is initialized from the sequence of characters specified
+	 * in a provided string. The unique characters from this string are also used to
+	 * initialize an LZWDictionary maintained and used by the LZWCompressor when
+	 * encoding/decoding
+	 * </p>
+	 * 
+	 * 
+	 * 
+	 * @param input a string representing an input sequence of characters to be
+	 *              encoded/decoded
+	 * 
+	 * @throws an IllegalArgumentException if the input string is empty
+	 * 
+	 */
+	public LZWCompressor(String input) {
+		if (input.contentEquals("")) {
+			throw new IllegalArgumentException();
+		}
+		this.input = input;
+		LZWDictionary dd = new LZWDictionary(input);
+		this.d = dd;
+	}
+
+	/**
+	 * Returns the original input sequence to be encoded/decoded by this
+	 * LZWCompressor
+	 * 
+	 * @return a string representing the original input sequence
+	 * 
+	 */
+	public String getInput() {
+
+		return this.input;
+
+	}
+
+	/**
+	 * Returns the dictionary used by this LZWCompressor
+	 * 
+	 * @return a reference to the LZWDictionary used by this LZWCompressor
+	 */
+	public LZWDictionary getDictionary() {
+
+		return this.d;
+
+	}
+
+	/**
+	 * Generates an LZW encoding of the input sequence
+	 * 
+	 * 
+	 * <p>
+	 * Uses the LZW encoding algorithm given in the lab4 specification
+	 * 
+	 * @return a list of integers representing the sequence of codes (indexes of
+	 *         character patterns) learned by an LZWDictionary during the encoding
+	 *         process
+	 * 
+	 */
+	public List<Integer> encode() {
+		List<String> single = new ArrayList<>();
+		List<Integer> ret = new ArrayList<>();
+		List<String> copy = new ArrayList<>();
+		String c = this.input.trim();
+		if (c.length() > 0) {
+
+			for (String s : this.d.lentries) {
+				copy.add(s);
+			}
+
+			for (int i = 0; i < c.length(); i++) {
+				single.add(Character.toString(this.input.charAt(i)));
+			}
+			String W = single.get(0);
+			int j = 0;
+			String WC = "";
+			while (j < single.size() - 1) {
+				j = j + 1;
+				String C = single.get(j);
+				WC = W + C;
+				if (copy.contains(WC)) {
+					W = WC;
+				} else {
+					ret.add(copy.indexOf(W));
+					copy.add(WC);
+					W = C;
+				}
+			}
+			ret.add(copy.indexOf(W));
+			this.enc = copy;
+			return ret;
+		}
+		return ret;
+	}
+
+	/**
+	 * Decodes an LZW encoding to generate the original input sequence
+	 * 
+	 * 
+	 * <p>
+	 * Uses the LZW decoding algorithm given in the lab4 specification
+	 * 
+	 * @param encoded a list of integers representing a sequence of codes (indexes
+	 *                of character patterns) learned by an LZWDictionary during the
+	 *                encoding process
+	 * 
+	 * @return a string representation of the decoded input sequence
+	 * 
+	 * @throws an IllegalArgumentException if encoded is an empty list
+	 * 
+	 */
+	public String decode(List<Integer> encoded) {
+
+		if (encoded.size() == 0) {
+			throw new IllegalArgumentException();
+		}
+		this.encode();
+		String in = "";
+		List<String> copy1 = new ArrayList<>();
+		for (String s : this.d.lentries) {
+			copy1.add(s);
+		}
+		String PREV = this.enc.get(0);
+		in += PREV;
+		String S = "";
+		String C = "";
+		int j = 0;
+		while (j < encoded.size() - 1) {
+			j = j + 1;
+			String NEXT = this.enc.get(encoded.get(j));
+			if (copy1.contains(NEXT)) {
+				S = NEXT;
+			} else {
+				S = PREV;
+				C = Character.toString(S.charAt(0));
+				S = S + C;
+			}
+			in += S;
+			String ENTRY = PREV + Character.toString(S.charAt(0));
+			copy1.add(ENTRY);
+			PREV = NEXT;
+		}
+		return in;
+	}
+
+	/**
+	 * Returns the compression ratio of an encoding
+	 * 
+	 * 
+	 * <p>
+	 * The compression ration (CR) is defined as the number of characters in the
+	 * input sequence, divided by the number of codes in the encoded version of the
+	 * input sequence
+	 * </p>
+	 * 
+	 * 
+	 * @return a double representing the compression ratio
+	 * 
+	 */
+	public double compressionRatio() {
+		String c = this.input.trim();
+		if (c.length() == 0) {
+			return 1;
+		}
+
+		LZWCompressor n = new LZWCompressor(this.input);
+		List<Integer> enc = n.encode();
+		return (double) c.length() / (double) enc.size();
+
+	}
+
+	/**
+	 * Some simple test cases that can be run independently of the junit tester
+	 * 
+	 */
+	public static void main(String[] args) {
+
+		LZWCompressor codec = new LZWCompressor("ababababa");
+
+		// codec = new LZWCompressor("#@$*@#($*@#$@(#*$@(#*$@#$");
+		// codec = new LZWCompressor("the fat the cat the bat the rat the mat the sat
+		// the tat");
+		// codec = new LZWCompressor("1231411212312312312124312413");
+		// codec = new
+		// LZWCompressor("thefatthecatthebattheratthematthesatthetatthefatthecatthebattheratthematthesatthe");
+
+		// ENCODE
+		System.out.println("original input sequence: " + codec.getInput());
+		List<Integer> enc = codec.encode();
+		System.out.println("encoded sequence: " + enc);
+		System.out.println("compression ratio: " + codec.compressionRatio());
+		System.out.println("-------------------");
+
+		// DECODE
+		String dec = codec.decode(enc);
+		System.out.println("decoded sequence: " + dec);
+		System.out.println("successful decode = " + dec.equals(codec.getInput()));
+
+	}
+}
